d2176dd3fa5ed3b7226eb637d153bfa644d59504
Haim-hub <niklas@haim.dk>
2023-11-10 13:23:11 +0800

    Minor refactoring

--- app/src/main/java/com/example/moodmetrics/MoodFragment.java
+++ app/src/main/java/com/example/moodmetrics/MoodFragment.java
@@ -1,38 +1,21 @@
 package com.example.moodmetrics;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public class MoodFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private TextView sad, saddest, neutral, happy, happiest, date;
-
+    private final String username;
+    private String mood;
     private Button submit;
-
-    private String username;
-
 
     public MoodFragment(String u) {
         this.username = u;
@@ -41,80 +24,46 @@ public class MoodFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-
-
-
-
     }
 
+    @SuppressLint("SimpleDateFormat")
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_mood, container, false);
 
-        final String[] mood = new String[1];
-        saddest = (TextView) view.findViewById(R.id.saddestSmiley);
-        sad     = (TextView) view.findViewById(R.id.sadSmiley);
-        neutral = (TextView) view.findViewById(R.id.neutralSmiley);
-        happy   = (TextView) view.findViewById(R.id.happySmiley);
-        happiest= (TextView) view.findViewById(R.id.happiestSmiley);
-        submit  = (Button)   view.findViewById(R.id.submit);
-        date    = (TextView) view.findViewById(R.id.date);
-        date.setText(new SimpleDateFormat("dd. MMMM yyyy - hh:mm").format(new Date()));
+        submit   = (Button)   view.findViewById(R.id.submit);
+        TextView saddest  = (TextView) view.findViewById(R.id.saddestSmiley);
+        TextView sad      = (TextView) view.findViewById(R.id.sadSmiley);
+        TextView neutral  = (TextView) view.findViewById(R.id.neutralSmiley);
+        TextView happy    = (TextView) view.findViewById(R.id.happySmiley);
+        TextView happiest = (TextView) view.findViewById(R.id.happiestSmiley);
+        TextView dateView = (TextView) view.findViewById(R.id.date);
 
+        dateView.setText(new SimpleDateFormat("dd. MMMM yyyy - hh:mm").format(new Date()));
 
-        saddest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mood[0] = "1";
-                submit.setEnabled(true);
-            }
-        });
-        sad.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mood[0] = "2";
-                submit.setEnabled(true);
-            }
-        });
-        neutral.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mood[0] = "3";
-                submit.setEnabled(true);
-            }
-        });
-        happy.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mood[0] = "4";
-                submit.setEnabled(true);
-            }
-        });
-        happiest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mood[0] = "5";
-                submit.setEnabled(true);
-            }
-        });
+        setMoodButtonListener(saddest, "1");
+        setMoodButtonListener(sad, "2");
+        setMoodButtonListener(neutral, "3");
+        setMoodButtonListener(happy, "4");
+        setMoodButtonListener(happiest, "5");
 
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                DBHelper dbHelper = DBHelper.getInstance(getContext());
-                Date date = new Date();
-                dbHelper.addMoodEntryToDB(username, mood[0], date);
-                submit.setEnabled(false);
-            }
+        submit.setOnClickListener(v -> {
+            DBHelper dbHelper = DBHelper.getInstance(getContext());
+            Date date = new Date();
+            dbHelper.addMoodEntryToDB(username, mood, date);
+            submit.setEnabled(false);
         });
 
         return view;
     }
+
+    private void setMoodButtonListener(TextView b, String moodValue) {
+        b.setOnClickListener(v -> {
+            mood = moodValue;
+            submit.setEnabled(true);
+        });
+    }
+
+
 }
